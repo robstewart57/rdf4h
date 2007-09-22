a83d9db9220f49d3ca7fea3ea12404e4929e6f43
Calvin Smith <cs-code@protempore.net>
2007-09-22 09:24:18 +0100

    Changed main to use new triplesOf method, since parsing now returns a Graph rather than a list of triples.
    darcs-hash:20070922082418-db1bb-f35b7da8a436c470490e9ebce5900bbd5a0e5fda

--- NTriplesMain.hs
+++ NTriplesMain.hs
@@ -1,9 +1,10 @@
 import System.Environment
 import NTriples
+import RDF
 
 -- |A dumb main that just dumps the triples to stdout. Will add more later.
 main = 
   getArgs >>= \t -> (parseFile $ head t) >>= \res ->
     case (res) of
       Left err -> print $ show err
-      Right xs -> mapM_ (putStrLn . show) xs
+      Right xs -> mapM_ (putStrLn . show) (triplesOf xs)
