7e556c37adfb4aab67afde9e96ab19d4ab0e09e5
Calvin Smith <cs-code@protempore.net>
2007-09-23 02:18:17 +0100

    Added some convenience functions for selecting and manipulating nodes and triples.
    darcs-hash:20070923011817-db1bb-ce6cd07176be3a68db9c434f8f72c87eb7f382af

--- RDF.hs
+++ RDF.hs
@@ -2,10 +2,16 @@ module RDF (Graph(Graph), triplesOf,
             Triple, triple,
             Node(UNode, BNode, LNode),
             LValue(PlainL, TypedL),
-            Selector, select, isUNode, isBNode, isLNode)
+            Selector, select, isUNode, isBNode, isLNode,
+              hasSubject, hasPredicate, hasObject,
+            subjectOf, predicateOf, objectOf, distinct,
+            printT, printTs, printN, printNs)
 where
 
 import Namespace
+
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Text.Printf
 
 newtype Graph = Graph [Triple]
@@ -13,17 +19,18 @@ newtype Graph = Graph [Triple]
 data Node =  UNode String               -- a uri ref node
            | BNode String               -- a blank node
            | LNode LValue               -- a literal node
-  deriving (Eq)
+  deriving (Eq, Ord)
 
+triplesOf :: Graph -> [Triple]
 triplesOf (Graph ts) = ts
 
 data Triple = Triple Node Node Node
   deriving (Eq)
 
+-- TODO: check spec for equality and comparison of literals
 data LValue = PlainL String (Maybe String) -- a plain literal w/ opt lang
             | TypedL String String         -- a typed literal w/ type uri
-
-  deriving (Eq)
+  deriving (Eq, Ord)
 
 triple :: Node -> Node -> Node -> Triple
 -- subject must be U or B
@@ -48,18 +55,41 @@ instance Show Node where
   show (LNode (PlainL lit (Just lang))) = printf "\"%s\"@\"%s\"" lit lang
   show (LNode (TypedL lit uri))         = printf "\"%s\"^^<%s>" lit uri
 
+-- Functions for testing type of a node
+isUNode, isBNode, isLNode :: Node -> Bool
 isUNode (UNode _) = True
 isUNode _         = False
-
 isBNode (BNode _) = True
 isBNode _         = False
-
 isLNode (LNode _) = True
 isLNode _         = False
 
--- Simplest possible selector
-type Selector = Node -> Node -> Node -> Bool
+-- Functions for extracting a node from a triple
+subjectOf, predicateOf, objectOf :: Triple -> Node
+subjectOf    (Triple s _ _) = s
+predicateOf  (Triple _ p _) = p
+objectOf     (Triple _ _ o) = o
 
+-- Eliminates duplicates from a list.
+distinct :: Ord a => [a] -> [a]
+distinct = Set.toList . Set.fromList
+
+-- Simplest possible selector: is passed each triple and says yay or nay
+type Selector = Node -> Node -> Node -> Bool
 select :: Selector -> Graph -> [Triple]
 select sl (Graph ts) = filter (\(Triple s p o) -> sl s p o) ts
 
+-- Convenience functions for user with select.
+hasSubject, hasPredicate, hasObject :: Node -> Node -> Node -> Node -> Bool
+hasSubject     u@(UNode _)  s _ _   =   u == s
+hasSubject     b@(BNode _)  s _ _   =   b == s
+hasSubject       (LNode _)  _ _ _   =   False
+hasPredicate   u@(UNode _)  _ p _   =   u == p
+hasPrecicate     _          _ _ _   =   False
+hasObject      u            _ _ o   =   u == o
+
+-- Utility functions for interactive experimentation
+printT  = putStrLn . show :: Triple -> IO ()
+printTs = mapM_ printT
+printN  = putStrLn . show :: Node   -> IO ()
+printNs = mapM_ printN
