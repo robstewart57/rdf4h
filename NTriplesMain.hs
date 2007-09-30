098da1b6386c186179257fb5baae5472dc93f2d2
Calvin Smith <cs-code@protempore.net>
2007-09-30 09:17:56 +0100

    NTriplesMain.hs: changed to use AvlGraph instead of TriplesGraph.
    darcs-hash:20070930081756-db1bb-90488745cfc42e8b51cf08a50b0798d4153b1aa8

--- NTriplesMain.hs
+++ NTriplesMain.hs
@@ -1,11 +1,11 @@
 import System.Environment
 import NTriples
 import RDF
-import TriplesGraph
+import AvlGraph
 
 -- |A dumb main that just dumps the triples to stdout. Will add more later.
 main = 
   getArgs >>= \t -> (parseFile $ head t) >>= \res ->
     case (res) of
       Left err -> print $ show err
-      Right gr -> mapM_ (putStrLn . show) (triplesOf (gr :: TriplesGraph))
+      Right gr -> mapM_ (putStrLn . show) (triplesOf (gr :: AvlGraph))
