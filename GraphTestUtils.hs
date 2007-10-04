0b34a18c81c9687ebd0dd0cf82529d33f1e3cad5
Calvin Smith <cs-code@protempore.net>
2007-10-04 05:49:46 +0100

    GraphtTestUtils.hs: added more tests.
    darcs-hash:20071004044946-db1bb-280bb032b59a3f4d0203ccaf71f7f443bb90c26b

--- GraphTestUtils.hs
+++ GraphTestUtils.hs
@@ -42,8 +42,8 @@ p_query_all_wildcard  _mkGraph ts = uordered ts == ordered result
 
 -- query with no wildcard and a triple in the graph should yield
 -- a singleton list with just the triple.
-p_query_matched_no_wildcards :: Graph g => (g -> Triples) -> g -> Property
-p_query_matched_no_wildcards _triplesOf gr = 
+p_query_matched_spo :: Graph g => (g -> Triples) -> g -> Property
+p_query_matched_spo _triplesOf gr = 
   classify (null ts) "trivial" $ 
     forAll (tripleFromGen _triplesOf gr) f
   where 
@@ -53,8 +53,8 @@ p_query_matched_no_wildcards _triplesOf gr =
             (Just t') ->  [t'] == queryT gr t'
 
 -- query with no wildcard and a triple no in the graph should yield []
-p_query_unmatched_no_wildcards :: Graph g => (g -> Triples) -> g -> Triple -> Property
-p_query_unmatched_no_wildcards _triplesOf gr t = 
+p_query_unmatched_spo :: Graph g => (g -> Triples) -> g -> Triple -> Property
+p_query_unmatched_spo _triplesOf gr t = 
   classify (t `elem` ts) "ignored" $
     not (t `elem` ts) ==> [] == queryT gr t
   where 
@@ -63,8 +63,44 @@ p_query_unmatched_no_wildcards _triplesOf gr t =
 -- query with fixed subject and wildcards for pred and obj should yield
 -- a list with all triples having subject, and graph minus result triples
 -- should yield all triple with unequal subjects.
-p_query_matched_po_wildcards :: Graph g => (g -> Triples) -> g -> Property
-p_query_matched_po_wildcards _triplesOf gr =
+p_query_match_s :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_s = mk_query_match_fn sameSubj f
+  where f t = (Just (subjectOf t), Nothing, Nothing)
+
+-- query w/ fixed predicate and wildcards for subj and obj should yield
+-- a list with all triples having predicate, and graph minus result triples
+-- should yield all triple with unequal predicates.
+p_query_match_p :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_p = mk_query_match_fn samePred f
+  where f t = (Nothing, Just (predicateOf t), Nothing)
+
+-- likewise for fixed subject and predicate with object wildcard
+p_query_match_o :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_o = mk_query_match_fn sameObj f 
+  where f t = (Nothing, Nothing, Just (objectOf t))
+
+-- verify likewise for fixed subject and predicate with wildcard object
+p_query_match_sp :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_sp = mk_query_match_fn same f
+  where same t1 t2 = sameSubj t1 t2 && samePred t1 t2
+        f t = (Just $ subjectOf t, Just $ predicateOf t, Nothing)
+
+-- fixed subject and object with wildcard predicate
+p_query_match_so :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_so = mk_query_match_fn same f
+  where same t1 t2 = sameSubj t1 t2 && sameObj t1 t2
+        f t = (Just $ subjectOf t, Nothing, Just $ objectOf t)
+
+-- fixed predicate and object with wildcard subject
+p_query_match_po :: Graph g => (g -> Triples) -> g -> Property
+p_query_match_po = mk_query_match_fn same f
+  where same t1 t2 = samePred t1 t2 && sameObj t1 t2
+        f t = (Nothing, Just $ predicateOf t, Just $ objectOf t)
+
+mk_query_match_fn :: Graph g => (Triple -> Triple -> Bool)
+  -> (Triple -> (Maybe Node, Maybe Node, Maybe Node))
+  -> (g -> Triples) -> g -> Property
+mk_query_match_fn tripleCompareFn  mkPatternFn _triplesOf gr =
   forAll (tripleFromGen _triplesOf gr) f
   where
     f :: Maybe Triple -> Bool
@@ -73,13 +109,24 @@ p_query_matched_po_wildcards _triplesOf gr =
       let 
         all_ts = _triplesOf gr
         all_ts_sorted = ordered all_ts
-        results = ordered (query gr (Just $ subjectOf t) Nothing Nothing)
+        results = ordered $ queryC gr (mkPatternFn t)
         notResults = ldiff all_ts_sorted results        
       in
-        all (sameSubj t) results &&
-        all (not . sameSubj t) notResults
-
+        all (tripleCompareFn t) results &&
+        all (not . tripleCompareFn t) notResults
+ 
 -- Utility functions and test data ... --
+
+-- a curried version of query that delegates to the actual query after unpacking
+-- curried maybe node pattern.
+queryC :: Graph g => g -> (Maybe Node, Maybe Node, Maybe Node) -> Triples
+queryC gr (s, p, o) = query gr s p o
+
+uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
+uncurry3 fn = \(x,y,z) -> fn x y z
+
+curry3 :: ((a, b, c) -> d)    ->     (a -> b -> c -> d)
+curry3 fn = \x -> \y -> \z -> fn (x,y,z)
 
 debug :: String -> Triples -> Bool
 debug msg ts = unsafePerformIO $ 
