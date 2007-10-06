a36cec956eaa4d8094f8a18ca4dd4a98496de31a
Calvin Smith <cs-code@protempore.net>
2007-10-06 19:25:48 +0100

    AvlGraph_Test.hs: added tests for select function.
    darcs-hash:20071006182548-db1bb-b47b1162e2ca5467caef26ddc527861a75a85a3c

--- AvlGraph_Test.hs
+++ AvlGraph_Test.hs
@@ -41,8 +41,8 @@ prop_mkGraph_triplesOf = p_mkGraph_triplesOf _triplesOf _mkGraph
 prop_mkGraph_no_dupes :: Triples -> Bool
 prop_mkGraph_no_dupes = p_mkGraph_no_dupes _triplesOf _mkGraph
 
-prop_query_all_wildcard :: Triples -> Bool
-prop_query_all_wildcard = p_query_all_wildcard _mkGraph
+prop_query_match_none :: Triples -> Bool
+prop_query_match_none = p_query_match_none _mkGraph
 
 prop_query_matched_spo :: AvlGraph -> Property
 prop_query_matched_spo = p_query_matched_spo _triplesOf
@@ -67,3 +67,27 @@ prop_query_match_so = p_query_match_so _triplesOf
 
 prop_query_match_po :: AvlGraph -> Property
 prop_query_match_po = p_query_match_po _triplesOf
+
+prop_select_match_none :: AvlGraph -> Bool
+prop_select_match_none = p_select_match_none
+
+prop_select_match_s :: AvlGraph -> Property
+prop_select_match_s = p_select_match_s _triplesOf
+
+prop_select_match_p :: AvlGraph -> Property
+prop_select_match_p = p_select_match_p _triplesOf
+
+prop_select_match_o :: AvlGraph -> Property
+prop_select_match_o = p_select_match_o _triplesOf
+
+prop_select_match_sp :: AvlGraph -> Property
+prop_select_match_sp = p_select_match_sp _triplesOf
+
+prop_select_match_so :: AvlGraph -> Property
+prop_select_match_so = p_select_match_so _triplesOf
+
+prop_select_match_po :: AvlGraph -> Property
+prop_select_match_po = p_select_match_po _triplesOf
+
+prop_select_match_spo :: AvlGraph -> Property
+prop_select_match_spo = p_select_match_spo _triplesOf
