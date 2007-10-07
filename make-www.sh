5e1c0170d56d957b9e69ab922601fc8a777cef1c
Calvin Smith <cs-code@protempore.net>
2007-10-07 01:20:43 +0100

    Fixed some problems with make-www.sh script.
    darcs-hash:20071007002043-72ac0-22cea996f710c0ad9c474f886a313a6f6711cbe2

--- make-www.sh
+++ make-www.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-pushd $(dirname $0) $>/dev/null
-ln -sf www/index.xhtml
-ln -sf dist/doc/html doc
-ln -sf dist/rdf4h*.tar.gz
-popd &>/dev/null
+if [ "$(pwd)" != "/home/calvins/rdf4h" ]
+then
+    echo "Must run from rdf4h directory."
+    exit 1
+fi
+ln -sf ./www/index.xhtml
+ln -sf ./dist/doc/html/ doc
+pushd www &> /dev/null
+ln -sf ../dist/rdf4h*.tar.gz 
+popd &> /dev/null
