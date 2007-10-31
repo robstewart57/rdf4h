9bcd3984301330caab5916de424e6fd03757593a
Calvin Smith <cs-code@protempore.net>
2007-10-31 01:15:57 +0000

    Utils.hs: a module containing utility stuff, currently comprised of a FastString and associated functions.
    darcs-hash:20071031011557-db1bb-deae76208425d571cc41d94a9449091fbaaad7e6

---
+++ Utils.hs
@@ -0,0 +1,63 @@
+module Utils where
+
+import Data.ByteString.Char8(ByteString)
+import qualified Data.ByteString.Char8 as B
+
+import Control.Monad
+import Data.HashTable(HashTable)
+import qualified Data.HashTable as HT
+import Data.Int(Int32, Int64)
+import Data.Char(ord)
+import Data.Bits(shiftR)
+import Data.IORef
+
+-- |A wrapper around a bytestring that contains a uniq int associated with the bytestring,
+-- together with the length of the bytestring and the value of the bytestring
+data FastString = FS {
+      uniq   :: {-# UNPACK #-} !Int,
+      len    :: {-# UNPACK #-} !Int,
+      value  :: {-# UNPACK #-} !ByteString
+}
+
+instance Show FastString where
+  show = B.unpack . B.reverse . value
+
+instance Eq FastString where
+  (FS u1 _ _) == (FS u2 _ _) = u1 == u2
+
+instance Ord FastString where
+  compare (FS _ _ v1) (FS _ _ v2) = compare v1 v2
+
+fsCounter :: IO (IORef Int)
+fsCounter = newIORef 0
+
+-- The hashfunction is adapted for bytestring but otherwise copied from Data.HashTable.
+fsTable :: IO (HashTable ByteString FastString)
+fsTable = HT.new (==) (B.foldl f 0)
+  where f m c = fromIntegral (ord c + 1) * golden + mulHi m golden
+        golden =  -1640531527 :: Int32
+        -- hi 32 bits of a x-bit * 32 bit -> 64-bit multiply
+        mulHi :: Int32 -> Int32 -> Int32
+        mulHi a b = fromIntegral (r `shiftR` 32)
+          where r :: Int64
+                r = fromIntegral a * fromIntegral b :: Int64
+
+mkFastString :: ByteString -> IO FastString
+mkFastString str = mkFastString' fsTable str
+
+mkFastString' :: IO (HashTable ByteString FastString) -> ByteString -> IO FastString
+mkFastString' fst str =
+  do ht <- fst
+     res <- HT.lookup ht str
+     case res of
+       Nothing   -> do fs <- newFastString str
+                       HT.insert ht str fs
+                       return fs
+       (Just fs)  -> return fs
+
+newFastString :: ByteString -> IO FastString
+newFastString str = 
+  do curr <- readIORef =<< fsCounter
+     fsCounter >>= flip writeIORef (curr+1)
+     return $ FS curr (B.length str) (B.reverse str)
+
