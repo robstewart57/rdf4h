23d27b00d5d648867345db6c560ffd5a01b8b82c
Calvin Smith <cs-code@protempore.net>
2007-11-05 21:42:22 +0000

    NTriplesParser.hs: added type signatures to get rid of compiler warnings.
    darcs-hash:20071105214222-db1bb-c84a680491af3e816ebaeef2158cd9b62fd35c10

--- NTriplesParser.hs
+++ NTriplesParser.hs
@@ -60,6 +60,8 @@ character 	::= 	[#x20-#x7E] /* US-ASCII space to decimal 126 */
 -- nt_ntripleDoc).
 
 -- |nt_ntripleDoc is simply zero or more lines.
+--nt_ntripleDoc = GenParser Char st (Maybe 
+nt_ntripleDoc :: GenParser Char st ([Maybe (IO Triple)], Maybe BaseUrl, PrefixMappings)
 nt_ntripleDoc = 
   many nt_line >>= \lines ->
   return (lines, Nothing, Map.empty)
@@ -69,6 +71,7 @@ nt_ntripleDoc =
 -- above. It encodes that the comment or triple are optional, as given in
 -- the EBNF. Parsec did not like having optional stuff after the skipMany,
 -- and this was a workaround.
+nt_line :: GenParser Char st (Maybe (IO Triple))
 nt_line      = do skipMany nt_space
                   res <- (nt_comment <|>  nt_triple <|> nt_empty)
                   nt_eoln
@@ -80,11 +83,13 @@ nt_line      = do skipMany nt_space
 -- is already defined as the range #x0020-#x007E, so cr #x000D and 
 -- lf #x000A are both already excluded. This returns Nothing as we are
 -- ignoring comments for now.
+nt_comment :: GenParser Char st (Maybe (IO Triple))
 nt_comment   = do char '#'; skipMany nt_character; return Nothing;
 
 -- A triple consists of whitespace-delimited subject, predicate, and object,
 -- followed by optional whitespace and a period, and possibly more 
 -- whitespace.
+nt_triple :: GenParser Char st (Maybe (IO Triple))
 nt_triple    = 
   do
     subj <- nt_subject
@@ -99,15 +104,18 @@ nt_triple    =
 
 -- nt_empty is a line that isn't a comment or a triple. They appear in the 
 -- parsed output as Nothing, whereas a real triple appears as (Just triple).
+nt_empty :: GenParser Char st (Maybe (IO Triple))
 nt_empty     = do skipMany nt_space; return Nothing
 
 -- A subject is either a URI reference for a resource or a node id for a
 -- blank node.
+nt_subject :: GenParser Char st (IO Node)
 nt_subject   = do {uri <- nt_uriref; return (unode uri);} <|> 
                do {nodeId <- nt_nodeID; return (bnode nodeId);}
 
 
 -- A predicate may only be a URI reference to a resource.
+nt_predicate :: GenParser Char st (IO Node)
 nt_predicate = do uri <- nt_uriref; return (unode uri);
 
 -- An object may be either a resource (represented by a URI reference),
@@ -118,10 +126,12 @@ nt_object = do {uri    <- nt_uriref;  return (unode uri) } <|>
             do {lit    <- nt_literal; return (lit >>= return . LNode)}
 
 -- A URI reference is an absolute URI inside angle brackets.
-nt_uriref = do char '<'; uri <- nt_absoluteURI; char '>'; return $ s2b uri
+nt_uriref :: GenParser Char st (ByteString)
+nt_uriref = do char '<'; uri <- nt_absoluteURI; char '>'; return uri
 
 -- A node id is "_:" followed by a name.
-nt_nodeID = do string "_:"; n <- nt_name; return $ s2b ('_':':':n)
+nt_nodeID :: GenParser Char st (ByteString)
+nt_nodeID = do string "_:"; n <- nt_name; return $ s2b "_:" `B.append` n
 
 -- A literal is either a language literal (with optional language
 -- specified) or a datatype literal (with required datatype
@@ -140,61 +150,74 @@ nt_literal    =
           do {return Nothing}
     case rt of
       Nothing              -> return (return $! PlainL str)
-      (Just (Left lng))    -> return (return $! PlainLL str (s2b lng))
+      (Just (Left lng))    -> return (return $! PlainLL str lng)
       (Just (Right uri))   -> return (mkFastString uri >>= \u -> return (TypedL str u))
 
 -- A language specifier of a language literal is any number of lowercase
 -- letters followed by any number of blocks consisting of a hyphen followed
 -- by one or more lowercase letters or digits.
+nt_language :: GenParser Char st (ByteString)
 nt_language    =   
   do
-    str1 <- many1 lower; 
+    str1 <- many1 lower >>= return . s2b; 
     str2 <- many (do { char '-'; 
                        ld <- many (lower <|> digit); 
-                       return ('-':ld); 
+                       return (s2b $ '-':ld); 
                      })
-    return (foldr1 (++) (str1:str2))
+    return (B.concat (str1:str2))
 
 -- End-of-line consists of either lf or crlf. We left-factored the pattern
 -- here to avoid using the try combinator.
+nt_eoln :: GenParser Char st ()
 nt_eoln        =   do  { nt_lf <|> nt_cr; optional nt_lf; }
 
 -- Whitespace is either a space or tab character. We must avoid using the
 -- built-in space combinator here, because it includes newline.
+nt_space :: GenParser Char st Char
 nt_space       =   char ' ' <|> nt_tab
 
 -- Carriage return is \r, given here as a Unicode escape.
+nt_cr :: GenParser Char st Char
 nt_cr          =   char '\x000D'
 
 -- Line feed is \n, given here as a Unicode escape.
+nt_lf :: GenParser Char st Char
 nt_lf          =   char '\x000A'
 
 -- Tab is \t, given here as a Unicode escape.
+nt_tab :: GenParser Char st Char
 nt_tab         =   char '\x0009'
 
 -- A name is a letter followed by any number of alpha-numeric characters.
-nt_name        =   do ltr <- letter; str <- many alphaNum; return (ltr:str)
+nt_name :: GenParser Char st ByteString
+nt_name        =   do ltr <- letter; str <- many alphaNum; return $! s2b (ltr:str)
 
 -- An absolute URI is at least 1 nrab_character. We do not attempt to 
 -- validate the URI at all, so anything that parses is accepted.
-nt_absoluteURI =   many1 nrab_character
+nt_absoluteURI :: GenParser Char st ByteString
+nt_absoluteURI =   many1 nrab_character >>= return . s2b
 
 -- An nt_character is any character except a double quote character.
+nt_character :: GenParser Char st Char
 nt_character   =   satisfy is_nonquote_char
 
 -- A nrab_character is a character that isn't a right angle bracket (this
 -- is used where we are inside a URIref, where right angle brackets are
 -- not allowed).
+nrab_character :: GenParser Char st Char
 nrab_character =   satisfy (\c -> c /= '>' && is_character c)
 
 -- A character is any Unicode value from ASCII space to decimal 126 (tilde).
+is_character :: Char -> Bool
 is_character c =   c >= '\x0020' && c <= '\x007E'
 
 -- A non-quote character is a character that isn't the double-quote character.
+is_nonquote_char :: Char -> Bool
 is_nonquote_char c = is_character c && c/= '"'
 
 -- The nt_string is simply a bunch of inner_string parts concatenated.
-nt_string = do strs <- many inner_string; return $ B.concat $ map B.pack strs
+nt_string :: GenParser Char st ByteString
+nt_string =  many inner_string >>= return . B.concat
 
 -- An inner_string is a fragment of a string (this is used inside double 
 -- quotes), and consists of the non-quote characters allowed and the 
@@ -202,13 +225,14 @@ nt_string = do strs <- many inner_string; return $ B.concat $ map B.pack strs
 -- a newline (\n), a double-quote (\"), a 4-digit Unicode escape (\uxxxx 
 -- where x is a hexadecimal digit), and an 8-digit Unicode escape
 -- (\Uxxxxxxxx where x is a hexadecimaldigit).
+inner_string :: GenParser Char st ByteString
 inner_string   = 
   do {
        char '\\'; 
-       do {c <- oneOf ['t', 'r', 'n', '\\', '"']; return ('\\':c:[])}  <|>
-       do {char 'u'; chrs <- count 4 hexDigit; return ('\\':'u':chrs)} <|>
-       do {char 'U'; chrs <- count 8 hexDigit; return ('\\':'U':chrs)}
-  }  <|> do {c <- satisfy is_nonquote_char; return (c:[])} 
+       do {c <- oneOf ['t', 'r', 'n', '\\', '"']; return $ s2b ('\\':c:[])}  <|>
+       do {char 'u'; chrs <- count 4 hexDigit; return $ s2b ('\\':'u':chrs)} <|>
+       do {char 'U'; chrs <- count 8 hexDigit; return $ s2b ('\\':'U':chrs)}
+  }  <|> do {c <- satisfy is_nonquote_char; return (B.singleton c)} 
 
 -- ==========================================================
 -- ==  END OF PARSER COMBINATORS AND SUPPORTING FUNCTIONS  ==
