b88ac3bd7d233f7c13debf937a5960314bc94a02
Calvin Smith <cs-code@protempore.net>
2007-11-07 06:49:30 +0000

    TurtleParserMain.hs: added to parser main.
    darcs-hash:20071107064930-db1bb-1ffb2ffde76708d3194fd61a1b01062f5f704cff

--- TurtleParserMain.hs
+++ TurtleParserMain.hs
@@ -1,12 +1,22 @@
 import System.Environment
+import System.Exit
 import TurtleParser
 import RDF
 import MGraph
 
+
 main :: IO ()
 main = 
-  getArgs >>= \t -> (parseFile Nothing "" $ head t) >>= \res ->
-    case (res) of
-      Left err -> print $ show err
-      Right gr -> mapM_ (putStrLn . show) (triplesOf (gr :: MGraph))
+  getArgs >>= \args -> 
+    if length args /= 3
+       then usage >> exitFailure
+       else let baseUrl  = Just $ BaseUrl $ s2b $ args !! 0
+                docUrl   = args !! 1
+                filepath = args !! 2
+            in parseFile baseUrl docUrl filepath >>= \res ->
+              case (res) of
+                Left err -> print $ show err
+                Right gr -> mapM_ (putStrLn . show) (triplesOf (gr :: MGraph))
 
+usage :: IO ()
+usage = getProgName >>= \name -> putStrLn $ name ++ " baseUrl docUrl filepath"
