b231919fb536808dddb24387ac543fcb1074d24a
Calvin Smith <cs-code@protempore.net>
2007-11-24 01:26:39 +0000

    Rdf4hQueryMain.hs: beginnings of a query utility.
    darcs-hash:20071124012639-db1bb-f2eaa30759f8f7cc73a70358992127978d8db347

---
+++ Rdf4hQueryMain.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import Text.RDF.Core
+import Text.RDF.TriplesGraph
+import qualified Text.RDF.NTriplesParser     as NP
+import qualified Text.RDF.NTriplesSerializer as NS
+import qualified Text.RDF.TurtleParser       as TP
+import qualified Text.RDF.TurtleSerializer   as TS
+
+import Data.ByteString.Char8(ByteString)
+import qualified Data.ByteString.Char8 as B
+
+import System.Environment
+import System.IO
+import System.Exit
+import System.Console.GetOpt
+
+import Control.Monad
+
+import Data.List
+import Data.Char(isLetter)
+import Text.Printf(hPrintf)
+
+main :: IO ()
+main = putStrLn "Hello"
+
+data Flag
+ = Help | Version | Verbose
+ | InputFormat String | InputBaseUri String
+ deriving (Show)
+
+header :: String
+header =
+  "\nrdf4h_query: utility for querying RDF data\n\n"                         ++
+  "\nUsage: rdf4h_query [OPTION...] INPUT-URI [INPUT-BASE-URI]\n\n"          ++
+  "  INPUT-URI       a filename, URI or '-' for standard input (stdin).\n"   ++
+  "  INPUT-BASE-URI  the input/parser base URI or '-' for none.\n"           ++
+  "    Default is INPUT-URI\n"                                               ++
+  "    Equivalent to -I INPUT-BASE-URI, --input-base-uri INPUT-BASE-URI\n\n"
+
+options :: [OptDescr Flag]
+options =
+ [ Option ['h']  ["help"]                           (NoArg Help)   "Display this help, then exit"
+ , Option ['v']  ["verbose"]                     (NoArg Verbose)   "Display extra information messages to stderr"
+ , Option ['V']  ["version"]                     (NoArg Version)   "Show version number\n\n"
+
+ , Option ['i']  ["input"]        (ReqArg InputFormat  "FORMAT") $ "Set input format/parser to one of:\n" ++
+                                                                   "  turtle      Turtle (default)\n" ++
+                                                                   "  ntriples    N-Triples"
+ , Option ['I']  ["input-base-uri"]  (ReqArg InputBaseUri "URI") $ "Set the input/parser base URI. '-' for none.\n" ++
+                                                                   "  Default is INPUT-BASE-URI argument value.\n\n"
+
+ ]
+-- The current version of the executable, which for the moment is the same as
+-- the version for the library as a whole, as given in rdf4h.cabal.
+version :: String
+version = "0.4"
