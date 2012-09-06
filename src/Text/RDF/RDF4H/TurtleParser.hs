ae8e6f9576f0b91f8b301f739b154297952ef76d
Slava Kravchenko <slava.kravchenko@gmail.com>
2012-09-06 23:23:41 +0300

    deleted debugging stuff from the source files

--- src/Text/RDF/RDF4H/TurtleParser.hs
+++ src/Text/RDF/RDF4H/TurtleParser.hs
@@ -21,10 +21,6 @@ import qualified Data.Foldable as F
 import Data.Char (isDigit)
 import Control.Monad
 import Data.Maybe (fromMaybe)
-import Debug.Trace(trace)
-
--- To avoid compiler warnings when not being used.
-_trace = trace
 
 -- |An 'RdfParser' implementation for parsing RDF in the 
 -- Turtle format. It takes optional arguments representing the base URL to use
@@ -469,7 +465,6 @@ popPred :: GenParser ParseState Predicate
 popPred = getState >>= \(bUrl, dUrl, i, pms, ss, ps, cs, ts) ->
                 setState (bUrl, dUrl, i, pms, ss, tail ps, cs, ts) >>
                   when (null ps) (error "Cannot pop predicate off empty stack.") >>
-                  -- _trace (show ps) (return ()) >>
                   return (head ps)
 
 isInColl :: GenParser ParseState Bool
