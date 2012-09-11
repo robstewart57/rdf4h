e0b9d6e3d0e677513afe5a2cf90b560bb13b0f29
Slava Kravchenko <slava.kravchenko@gmail.com>
2012-09-11 15:02:34 +0300

    import TurtleParser_ConformanceTest as "qualified"

--- testsuite/tests/Test.hs
+++ testsuite/tests/Test.hs
@@ -3,10 +3,11 @@ module Main where
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Text.RDF.RDF4H.TurtleParser_ConformanceTest as TurtleParser
+
 import qualified Data.RDF.TriplesGraph_Test as TriplesGraph
 import qualified Data.RDF.MGraph_Test as MGraph
 import qualified Text.RDF.RDF4H.XmlParser_Test as XmlParser
+import qualified Text.RDF.RDF4H.TurtleParser_ConformanceTest as TurtleParser
 
 main :: IO () 
 main = defaultMain (  TriplesGraph.tests
