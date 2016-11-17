3265cbb4b4f9fa53e77ed871ace5f1eb859067f8
Alex Mason <Alex.Mason@data61.csiro.au>
2016-11-17 17:58:58 +1100

    Convert NTriplesParsers to use parsers library

--- src/Text/RDF/RDF4H/NTriplesParser.hs
+++ src/Text/RDF/RDF4H/NTriplesParser.hs
@@ -8,13 +8,16 @@ module Text.RDF.RDF4H.NTriplesParser
 import Prelude hiding (init,pred)
 import Data.RDF.Types
 import Text.RDF.RDF4H.ParserUtils
-import Data.Char (isLetter, isDigit,isAlphaNum)
+import Data.Char (isLetter, isDigit,isAlphaNum, isAsciiUpper, isAsciiLower)
 import Data.Map as Map (empty)
-import Text.Parsec
-import Text.Parsec.Text
+import Text.Parsec (runParser,ParseError)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Control.Monad (liftM,void,guard)
+import Control.Monad (void)
+
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Control.Applicative
 
 -- |NTriplesParser is an 'RdfParser' implementation for parsing RDF in the
 -- NTriples format. It requires no configuration options. To use this parser,
@@ -34,20 +37,20 @@ instance RdfParser NTriplesParser where
 -- nt_ntripleDoc).
 
 -- |nt_ntripleDoc is simply zero or more lines.
-nt_ntripleDoc :: GenParser () [Maybe Triple]
+nt_ntripleDoc :: (CharParsing m, Monad m) => m [Maybe Triple]
 nt_ntripleDoc = manyTill nt_line eof
 
-nt_line :: GenParser () (Maybe Triple)
+nt_line :: (CharParsing m, Monad m) => m (Maybe Triple)
 nt_line =
     skipMany nt_space >>
-     ((nt_comment >>= \res -> return res)
-      <|> try (nt_triple >>= \res -> nt_eoln >> return res)
-      <|> try (nt_triple >>= \res -> char '#' >> manyTill anyChar nt_eoln >> return res)
-      <|> (nt_empty >>= \res -> nt_eoln >> return res))
-     >>= \res -> return res
+     ((nt_comment >>= \res -> pure res)
+      <|> try (nt_triple >>= \res -> nt_eoln *> pure res)
+      <|> try (nt_triple >>= \res -> char '#' *> manyTill anyChar nt_eoln *> pure res)
+      <|> (nt_empty >>= \res -> nt_eoln *> pure res))
+     >>= \res -> pure res
 
-nt_comment :: GenParser () (Maybe Triple)
-nt_comment   = char '#' >> manyTill anyChar nt_eoln >> return Nothing
+nt_comment :: CharParsing m => m (Maybe Triple)
+nt_comment   = char '#' *> manyTill anyChar nt_eoln *> pure Nothing
 
 -- A triple consists of whitespace-delimited subject, predicate, and object,
 -- followed by optional whitespace and a period, and possibly more
@@ -60,147 +63,142 @@ nt_comment   = char '#' >> manyTill anyChar nt_eoln >> return Nothing
 --
 -- `optional` lets this nt_triple parser succeed even if there is not
 -- a space or tab character between resources or the object and the '.'.
-nt_triple :: GenParser () (Maybe Triple)
+nt_triple :: (CharParsing m, Monad m) => m (Maybe Triple)
 nt_triple    =
   do
     subj <- nt_subject
-    optional (skipMany1 nt_space)
+    optional (skipSome nt_space)
     pred <- nt_predicate
-    optional (skipMany1 nt_space)
+    optional (skipSome nt_space)
     obj <- nt_object
-    optional (skipMany1 nt_space)
+    optional (skipSome nt_space)
     void (char '.')
     void (many nt_space)
-    return $ Just (Triple subj pred obj)
+    pure $ Just (Triple subj pred obj)
 
 -- [6] literal
-nt_literal :: GenParser () LValue
+nt_literal :: (CharParsing m, Monad m) => m LValue
 nt_literal = do
   s' <- nt_string_literal_quote
   let s = escapeRDFSyntax s'
-  option (plainL s) $ do
-               ((count 2 (char '^') >> nt_iriref >>= validateURI >>= isAbsoluteParser >>= \iri -> return (typedL s iri))
-                <|> (nt_langtag >>= \lang -> return (plainLL s lang)))
+  option (plainL s) $
+               (count 2 (char '^') *> nt_iriref >>= validateURI >>= isAbsoluteParser >>= \iri -> pure (typedL s iri))
+                <|> (nt_langtag >>= \lang -> pure (plainLL s lang))
 
 -- [9] STRING_LITERAL_QUOTE
-nt_string_literal_quote :: GenParser () T.Text
+nt_string_literal_quote :: (CharParsing m, Monad m) => m T.Text
 nt_string_literal_quote =
-    between (char '"') (char '"') $ do
-      T.concat <$> (many ((T.singleton <$> noneOf ['\x22','\x5C','\xA','\xD']) <|>
+    between (char '"') (char '"') $
+      T.concat <$> many ((T.singleton <$> noneOf ['\x22','\x5C','\xA','\xD']) <|>
                           nt_echar <|>
-                          nt_uchar))
+                          nt_uchar)
 
 -- [144s] LANGTAG
-nt_langtag :: GenParser () T.Text
+nt_langtag :: (CharParsing m, Monad m) => m T.Text
 nt_langtag = do
   void (char '@')
-  ss   <- many1 (satisfy (\ c -> isLetter c))
-  rest <- concat <$> many (char '-' >> many1 (satisfy (\ c -> isAlphaNum c)) >>= \lang_str -> return ('-':lang_str))
-  return (T.pack (ss ++ rest))
+  ss   <- some (satisfy isLetter)
+  rest <- concat <$> many (char '-' *> some (satisfy isAlphaNum) >>= \lang_str -> pure ('-':lang_str))
+  pure (T.pack (ss ++ rest))
 
 -- [8] IRIREF
-nt_iriref :: GenParser () T.Text
-nt_iriref = do
-  between (char '<') (char '>') $ do
+nt_iriref ::(CharParsing m, Monad m) => m T.Text
+nt_iriref =
+  between (char '<') (char '>') $
               T.concat <$> many ( T.singleton <$> noneOf (['\x00'..'\x20'] ++ ['<','>','"','{','}','|','^','`','\\']) <|>
                                   nt_uchar )
 
 -- [153s] ECHAR
-nt_echar :: GenParser () T.Text
+nt_echar :: (CharParsing m, Monad m) => m T.Text
 nt_echar = try $ do
   void (char '\\')
-  c2 <- anyChar
-  guard $ isEchar c2
-  return (T.pack [c2])
+  c2 <- satisfy isEchar
+  pure (T.pack [c2])
 
 isEchar :: Char -> Bool
 isEchar = (`elem` ['t','b','n','r','f','"','\'','\\'])
 
 -- [10] UCHAR
-nt_uchar :: GenParser () T.Text
+nt_uchar :: (CharParsing m, Monad m) => m T.Text
 nt_uchar =
-    (try (char '\\' >> char 'u' >> count 4 hexDigit >>= \cs -> return $ T.pack ('\\':'u':cs)) <|>
-     try (char '\\' >> char 'U' >> count 8 hexDigit >>= \cs -> return $ T.pack ('\\':'U':cs)))
+    try (char '\\' *> char 'u' *> count 4 hexDigit >>= \cs -> pure $ T.pack ('\\':'u':cs)) <|>
+    try (char '\\' *> char 'U' *> count 8 hexDigit >>= \cs -> pure $ T.pack ('\\':'U':cs))
 
 -- nt_empty is a line that isn't a comment or a triple. They appear in the
 -- parsed output as Nothing, whereas a real triple appears as (Just triple).
-nt_empty :: GenParser () (Maybe Triple)
-nt_empty     = skipMany nt_space >> return Nothing
+nt_empty :: CharParsing m => m (Maybe Triple)
+nt_empty     = skipMany nt_space *> pure Nothing
 
 -- A subject is either a URI reference for a resource or a node id for a
 -- blank node.
-nt_subject :: GenParser () Node
+nt_subject :: (CharParsing m, Monad m) => m Node
 nt_subject   =
-  liftM unode nt_uriref <|>
-  liftM bnode nt_blank_node_label
+  fmap unode nt_uriref <|>
+  fmap bnode nt_blank_node_label
 
 -- A predicate may only be a URI reference to a resource.
-nt_predicate :: GenParser () Node
-nt_predicate = liftM unode nt_uriref
+nt_predicate :: (CharParsing m, Monad m) => m Node
+nt_predicate = fmap unode nt_uriref
 
 -- An object may be either a resource (represented by a URI reference),
 -- a blank node (represented by a node id), or an object literal.
-nt_object :: GenParser () Node
+nt_object :: (CharParsing m, Monad m) => m Node
 nt_object =
-  liftM unode nt_uriref <|>
-  liftM bnode nt_blank_node_label <|>
-  liftM LNode nt_literal
+  fmap unode nt_uriref <|>
+  fmap bnode nt_blank_node_label <|>
+  fmap LNode nt_literal
 
-validateUNode :: T.Text -> GenParser () Node
+validateUNode :: CharParsing m => T.Text -> m Node
 validateUNode t =
     case unodeValidate t of
       Nothing        -> unexpected ("Invalid URI in NTriples parser URI validation: " ++ show t)
-      Just u@(UNode{}) -> return u
+      Just u@UNode{} -> pure u
       Just node      -> unexpected ("Unexpected node in NTriples parser URI validation: " ++ show node)
 
-validateURI :: T.Text -> GenParser () T.Text
+validateURI :: (CharParsing m, Monad m) => T.Text -> m T.Text
 validateURI t = do
     UNode uri <- validateUNode t
-    return uri
+    pure uri
 
-isAbsoluteParser :: T.Text -> GenParser () T.Text
+isAbsoluteParser :: CharParsing m => T.Text -> m T.Text
 isAbsoluteParser t =
     if isAbsoluteUri t
-    then return t
+    then pure t
     else unexpected ("Only absolute IRIs allowed in NTriples format, which this isn't: " ++ show t)
 
-absoluteURI :: T.Text -> GenParser () T.Text
-absoluteURI t = do
-    uri <- isAbsoluteParser t
-    return uri
+absoluteURI :: CharParsing m => T.Text -> m T.Text
+absoluteURI = isAbsoluteParser
 
 -- A URI reference is one or more nrab_character inside angle brackets.
-nt_uriref :: GenParser () T.Text
+nt_uriref :: (CharParsing m, Monad m) => m T.Text
 nt_uriref = between (char '<') (char '>') $ do
               unvalidatedUri <- many (satisfy ( /= '>'))
               t <- validateURI (T.pack unvalidatedUri)
               absoluteURI t
 
 -- [141s] BLANK_NODE_LABEL
-nt_blank_node_label :: GenParser () T.Text
+nt_blank_node_label :: (CharParsing m, Monad m) => m T.Text
 nt_blank_node_label = do
-  void (char '_' >> char ':')
-  s1 <- (nt_pn_chars_u <|> satisfy isDigit)
+  void (char '_' *> char ':')
+  s1 <- nt_pn_chars_u <|> satisfy isDigit
   s2 <- option "" $ try $ do
           sub_dots <- many (char '.')
-          sub_s1   <- many1 nt_pn_chars
-          return (sub_dots ++ sub_s1)
-  return (T.pack ("_:" ++ [s1] ++ s2))
+          sub_s1   <- some nt_pn_chars
+          pure (sub_dots ++ sub_s1)
+  pure (T.pack ("_:" ++ [s1] ++ s2))
 
 -- [157s] PN_CHARS_BASE
 -- Not used. It used to be. What's happened?
 {-
-nt_pn_chars_base :: GenParser () Char
-nt_pn_chars_base = try $ do
-    c <- anyChar
-    guard $ isBaseChar c
-    return c
+nt_pn_chars_base :: CharParsing m => m Char
+nt_pn_chars_base = try $ satisfy isBaseChar c
+
 -}
 
 isBaseChar :: Char -> Bool
 isBaseChar c
-    =  (c >= 'A' && c <= 'Z')
-    || (c >= 'a' && c <= 'z')
+    =  isAsciiUpper c
+    || isAsciiLower c
     || (c >= '\x00C0' && c <= '\x00D6')
     || (c >= '\x00D8' && c <= '\x00F6')
     || (c >= '\x00F8' && c <= '\x02FF')
@@ -216,44 +214,40 @@ isBaseChar c
 
 
 -- [158s] PN_CHARS_U
-nt_pn_chars_u :: GenParser () Char
-nt_pn_chars_u = try $ do
-    c <- anyChar
-    guard $ c == '_' || c == ':' || isBaseChar c
-    return c
+nt_pn_chars_u :: CharParsing m => m Char
+nt_pn_chars_u = try $ satisfy $ \c -> c == '_' || c == ':' || isBaseChar c
 
 
 -- [160s] PN_CHARS
-nt_pn_chars :: GenParser () Char
-nt_pn_chars = nt_pn_chars_u <|> try (do
-    c <- anyChar
-    guard $ c == '-' || c == '\x00B7'
+nt_pn_chars :: CharParsing m => m Char
+nt_pn_chars = nt_pn_chars_u
+  <|> try (satisfy $ \c ->
+           c == '-' || c == '\x00B7'
            || isDigit c
            || (c >= '\x0300' && c <= '\x036F')
            || (c >= '\x203F' && c <= '\x2040')
-    return c
     )
 
 -- End-of-line consists of either lf or crlf.
 -- We also test for eof and consider that to match as well.
-nt_eoln :: GenParser () ()
-nt_eoln =  eof <|> void (nt_cr >> nt_lf) <|> void nt_lf
+nt_eoln :: CharParsing m => m ()
+nt_eoln =  eof <|> void (nt_cr *> nt_lf) <|> void nt_lf
 
 -- Whitespace is either a space or tab character. We must avoid using the
 -- built-in space combinator here, because it includes newline.
-nt_space :: GenParser () Char
+nt_space :: CharParsing m => m Char
 nt_space = char ' ' <|> nt_tab
 
--- Carriage return is \r.
-nt_cr :: GenParser () Char
+-- Carriage pure is \r.
+nt_cr :: CharParsing m => m Char
 nt_cr          =   char '\r'
 
 -- Line feed is \n.
-nt_lf :: GenParser () Char
+nt_lf :: CharParsing m => m Char
 nt_lf          =   char '\n'
 
 -- Tab is \t.
-nt_tab :: GenParser () Char
+nt_tab :: CharParsing m => m Char
 nt_tab         =   char '\t'
 
 parseString' :: (Rdf a) => T.Text -> Either ParseFailure (RDF a)
@@ -263,10 +257,10 @@ parseURL' :: (Rdf a) => String -> IO (Either ParseFailure (RDF a))
 parseURL' = _parseURL parseString'
 
 parseFile' :: (Rdf a) => String -> IO (Either ParseFailure (RDF a))
-parseFile' path = liftM (handleParse mkRdf . runParser nt_ntripleDoc () path)
+parseFile' path = fmap (handleParse mkRdf . runParser nt_ntripleDoc () path)
                    (TIO.readFile path)
 
-handleParse :: {-forall rdf. (RDF rdf) => -} (Triples -> Maybe BaseUrl -> PrefixMappings -> (RDF a)) ->
+handleParse :: {-forall rdf. (RDF rdf) => -} (Triples -> Maybe BaseUrl -> PrefixMappings -> RDF a) ->
                                         Either ParseError [Maybe Triple] ->
                                         Either ParseFailure (RDF a)
 handleParse _mkRdf result
