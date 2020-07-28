6a5a2dd51de86a6fe9fba7939d1892e33add268d
Tim McIver <tim@timmciver.com>
2020-07-27 22:42:40 -0400

    Give proper names to paths/fragments in test.

--- testsuite/tests/Data/RDF/TurtleSerializerTest.hs
+++ testsuite/tests/Data/RDF/TurtleSerializerTest.hs
@@ -12,7 +12,7 @@ tests :: TestTree
 tests =
   testGroup "findMappings Tests"
   [ testCase "findMapping correctly finds rdf mapping" $
-    assertEqual "" (Just ("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "blah")) (findMapping (coerce standard_ns_mappings) "rdf:blah")
+    assertEqual "" (Just ("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "subject")) (findMapping (coerce standard_ns_mappings) "rdf:subject")
   , testCase "findMapping correctly finds rdfs mapping" $
-    assertEqual "" (Just ("http://www.w3.org/2000/01/rdf-schema#", "blah")) (findMapping (coerce standard_ns_mappings) "rdfs:blah")
+    assertEqual "" (Just ("http://www.w3.org/2000/01/rdf-schema#", "domain")) (findMapping (coerce standard_ns_mappings) "rdfs:domain")
   ]
