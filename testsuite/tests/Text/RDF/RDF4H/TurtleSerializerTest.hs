5c44d0fb3b6cb78f3595aeed7c192f994f6cd0d9
Tim McIver <tim@timmciver.com>
2020-08-09 00:38:29 -0400

    Create failing test.

--- testsuite/tests/Text/RDF/RDF4H/TurtleSerializerTest.hs
+++ testsuite/tests/Text/RDF/RDF4H/TurtleSerializerTest.hs
@@ -2,17 +2,34 @@
 
 module Text.RDF.RDF4H.TurtleSerializerTest (tests) where
 
+import Data.Coerce
+import Data.RDF.Namespace
+import System.IO
+import System.IO.Temp (withSystemTempFile)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Text.RDF.RDF4H.TurtleSerializer
-import Data.RDF.Namespace
-import Data.Coerce
+
+force :: [a] -> ()
+force [] = ()
+force (_:xs) = force xs
 
 tests :: TestTree
-tests =
-  testGroup "findMappings Tests"
-  [ testCase "findMapping correctly finds rdf mapping" $
-    assertEqual "" (Just ("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "subject")) (findMapping (coerce standard_ns_mappings) "rdf:subject")
-  , testCase "findMapping correctly finds rdfs mapping" $
-    assertEqual "" (Just ("http://www.w3.org/2000/01/rdf-schema#", "domain")) (findMapping standard_ns_mappings "rdfs:domain")
+tests = testGroup "Turtle serializer tests"
+  [ testGroup "findMappings Tests"
+    [ testCase "findMapping correctly finds rdf mapping" $
+      assertEqual "" (Just ("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "subject")) (findMapping (coerce standard_ns_mappings) "rdf:subject")
+    , testCase "findMapping correctly finds rdfs mapping" $
+      assertEqual "" (Just ("http://www.w3.org/2000/01/rdf-schema#", "domain")) (findMapping standard_ns_mappings "rdfs:domain")]
+
+  , testGroup "writeUNodeUri tests"
+    [ testCase "should properly serialize a UNode" $
+        withSystemTempFile "rdf4h-"
+          (\_ h -> do
+              hSetBuffering h NoBuffering
+              writeUNodeUri h "rdf:subject" standard_ns_mappings
+              hSeek h AbsoluteSeek 0
+              contents <- hGetContents h
+              seq (force contents) (pure ())
+              "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject" @=? contents)]
   ]
