45114934a26e0828f810f907d02a5cedb0672231
Tim McIver <tim@timmciver.com>
2020-08-11 21:47:21 -0400

    Create failing test.
    This test reveals a bug where if triples with common subjects are separated by
    triples with different subjects, then those triples with common subjects are not
    grouped together as expected.

--- testsuite/tests/Text/RDF/RDF4H/TurtleSerializerTest.hs
+++ testsuite/tests/Text/RDF/RDF4H/TurtleSerializerTest.hs
@@ -3,12 +3,15 @@
 module Text.RDF.RDF4H.TurtleSerializerTest (tests) where
 
 import Data.ByteString as BS
+
 import Data.RDF.Namespace
+import Data.Function ((&))
+import Data.Map as Map
+import Data.RDF as RDF
 import System.IO
 import System.IO.Temp (withSystemTempFile)
 import Test.Tasty
 import Test.Tasty.HUnit
-import Text.RDF.RDF4H.TurtleSerializer
 
 tests :: TestTree
 tests = testGroup "Turtle serializer tests"
@@ -20,10 +23,33 @@ tests = testGroup "Turtle serializer tests"
 
   , testGroup "writeUNodeUri tests"
     [ testCase "should properly serialize a UNode" $
-        withSystemTempFile "rdf4h-"
-          (\_ h -> do
-              writeUNodeUri h "rdf:subject" standard_ns_mappings
-              hSeek h AbsoluteSeek 0
-              contents <- BS.hGetContents h
-              "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject" @=? contents)]
+      withSystemTempFile "rdf4h-"
+      (\_ h -> do
+          writeUNodeUri h "rdf:subject" standard_ns_mappings
+          hSeek h AbsoluteSeek 0
+          contents <- BS.hGetContents h
+          "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject" @=? contents)
+    ]
+
+  , testGroup "writeRdf tests"
+    [ testCase "triples with the same subject should be grouped" $
+      let g :: RDF TList
+          g = RDF.empty
+            & flip addTriple (triple (unode ":something") (unode "rdf:type") (unode "schema:Document"))
+            & flip addTriple (triple (unode ":another") (unode "rdf:type") (unode "schema:Document"))
+            & flip addTriple (triple (unode ":something") (unode "dc:title") (lnode (plainL "Some title")))
+          mappings = PrefixMappings $ Map.fromList [ ("schema", "http://schema.org/")
+                                                              , ("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+                                                              , ("schema", "http://schema.org/")]
+          serializer = TurtleSerializer Nothing mappings
+      in
+      withSystemTempFile "rdf4h-"
+      (\_ h -> do
+          expected <- BS.readFile "testsuite/tests/Text/RDF/RDF4H/data/common-subject.ttl"
+          hWriteRdf serializer h g
+          hSeek h AbsoluteSeek 0
+          actual <- BS.hGetContents h
+          BS.putStrLn actual
+          expected @=? actual)
+    ]
   ]
