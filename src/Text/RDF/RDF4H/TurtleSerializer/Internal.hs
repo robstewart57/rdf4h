b590bf9100afd11664b28fe44d0945fa73d936b1
Tim McIver <tim@timmciver.com>
2020-08-24 23:29:58 -0400

    Make failing test pass.

--- src/Text/RDF/RDF4H/TurtleSerializer/Internal.hs
+++ src/Text/RDF/RDF4H/TurtleSerializer/Internal.hs
@@ -4,7 +4,7 @@ module Text.RDF.RDF4H.TurtleSerializer.Internal
   )
 where
 
-import           Data.List (elemIndex)
+import           Data.List (elemIndex, isPrefixOf)
 import qualified Data.Map as Map
 import           Data.RDF.Namespace hiding (rdf)
 import qualified Data.Text as T
@@ -27,21 +27,14 @@ writeUNodeUri :: Handle         -- ^The Handle to write to
               -> T.Text         -- ^The text from a UNode
               -> PrefixMappings -- ^The 'PrefixMappings' which should contain a mapping for any prefix found in the URI.
               -> IO ()
-writeUNodeUri h uri pms =
+writeUNodeUri h uri _ =
+  if (isQName uri)
+  then T.hPutStr h uri
+  else hPutChar h '<' >> T.hPutStr h uri >> hPutChar h '>'
 
-  -- TODO: The following code determines if the URI text is a QName (see
-  -- https://www.w3.org/2007/02/turtle/primer) by checking to see if a mapping
-  -- for the QName's prefix exists in the given 'PrefixMappings'. If a mapping
-  -- exists, then it is assumed to be a QName. The problem with this approach is
-  -- that there may not be a mapping for the prefix in the given
-  -- 'PrefixMappings' (this is a separate issue that should be addressed)
-  -- leading this code to write the UNode with angle brackets when it should not
-  -- have them.
-  case mapping of
-    Nothing -> hPutChar h '<' >> T.hPutStr h uri >> hPutChar h '>'
-    _ -> T.hPutStr h uri
-  where
-    mapping = findMapping pms uri
+isQName :: T.Text -> Bool
+isQName uri = not ("http://" `isPrefixOf` uriStr || "https://" `isPrefixOf` uriStr)
+  where uriStr = T.unpack uri
 
 -- |Given an aliased URI (e.g., 'rdf:subject') return a tuple whose first
 -- element is the alias ('rdf') and whose second part is the path or fragment
